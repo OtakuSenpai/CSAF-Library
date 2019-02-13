516aa33a8be79ff863803e58a4770b6b53a33d95
OtakuSenpai <edu.avraneel@gmail.com>
2019-02-13 18:46:16 +0000

    Implemented selection sort in ascending and descending order, and merge sort in ascending.

--- algos/sort.hpp
+++ algos/sort.hpp
@@ -1,6 +1,8 @@
 #ifndef SORT_HPP
 #define SORT_HPP
 
+#include <cstdlib>
+
 template <typename T>
 void swapArray(T array[], T left, T right) {
   T temp = array[left];
@@ -54,15 +56,6 @@ void insertionSortAscending(T array[],size_t size) {
 }
 
 template <typename T>
-void insertionSortDescendingRecursive(T array[], size_t size, size_t start) {
-  if(start >= size - 1) return;
-  else {
-    insertInAscending(array,size,start);
-    insertionSortAscendingRecursive(array,size,start+1);
-  }
-}
-
-template <typename T>
 void insertInAscending(T array[], size_t size, size_t start) {
   size_t pos = start;
   while(pos > 0 && array[pos] < array[pos-1]) {
@@ -71,4 +64,59 @@ void insertInAscending(T array[], size_t size, size_t start) {
   }
 }
 
+template <typename T>
+void selectionSortAscending(T array[], size_t size) {
+  size_t temp;
+  for(size_t i = 0; i < size; i++) {
+    temp = i;
+    for(size_t j = temp + 1; j < size; j++) {
+      if(array[j] < array[temp])
+        temp = j;
+    }
+    swapArray(array,array[i],array[temp]);
+  }
+}
+
+template <typename T>
+void selectionSortDescending(T array[], size_t size) {
+  size_t temp;
+  for(size_t i = 0; i < size; i++) {
+    temp = i;
+    for(size_t j = temp + 1; j < size; j++) {
+      if(array[j] > array[temp]) {
+        temp = j;
+    }
+    swapArray(array,array[i],array[temp]);
+  }
+}
+
+template <typename T>
+void mergeSort(T input[], size_t left, size_t right, T output[]) {
+  if((right - left) == 1) output[0] = input[left];
+  else if(right - left > 1) {
+    size_t mid = (left+right)/2;
+    T leftArray[(mid-1) - left];
+    T rightArray[right - mid];
+    mergeSort(input,left,mid-1,leftArray);
+    mergeSort(input,mid,right,rightArray);
+    mergeAscending(leftArray,(mid-1) - left,rightArray,right - mid,output);
+  }
+}
+
+template <typename T>
+T[] mergeAscending(T array1[], size_t sizeArray1, T array2[], size_t sizeArray2, T total[]) {
+  size_t totalSize = sizeArray1 + sizeArray2;
+  size_t i,j,k;
+  while( k < i + j && k <= totalSize ) {
+    if(sizeArray1 == i || array1[i] <= array2[j]) {
+      total[k] = array1[i];
+      j++; k++;
+    } else if(sizeArray2 == j || array1[i] > array2[j]) {
+      total[k] = array2[j];
+      i++; k++;
+    }
+  }
+
+  return total;
+}
 #endif
---
+++ tests/algos/sort-test.cpp
@@ -0,0 +1,18 @@
+#include "../../algos/sort.hpp"
+
+using namespace std;
+
+int main() {
+  int array[] = {1,5,3,6,12,75,32,21,43};
+  int output[9];
+
+  cout << "Starting merge sort test..." << endl;
+
+  mergeSort(array,0,9,output);
+
+  for(int i = 0; i < 9; i++)
+    cout << output[i] << " ";
+
+  return 0;
+}
+}
